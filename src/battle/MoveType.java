abd01dc1ef4f10d62d4d6800c6f0dcdc5f650b5e
Yin4TheWin <36430386+Yin4TheWin@users.noreply.github.com>
2018-09-22 19:57:46 -0400

    Classes for Pokemon and their attributes

---
+++ src/battle/MoveType.java
@@ -0,0 +1,9 @@
+//From Jmgiacone's Java Pokemon Engine
+//https://github.com/Jmgiacone/Pokemon
+
+package battle;
+
+public enum MoveType
+{
+    PHYSICAL, SPECIAL, STATUS;
+}
---
+++ src/battle/Status.java
@@ -0,0 +1,16 @@
+package battle;
+
+public enum Status
+{
+    POISON,
+    PARALYZE,
+    BURN,
+    FREEZE,
+    SLEEP,
+    CONFUSE,
+    LOVE,
+    SEED,
+    ACCURACY,
+    SOAK,
+    FULL,
+}
